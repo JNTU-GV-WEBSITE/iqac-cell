18a972bf7c6d925b5fd6467a26c4afff6280b038
Kirtanaaa <uppalakirtana@gmail.com>
2023-12-27 19:06:23 +0530

    added header and navbar basic frame

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography } from '@mui/material';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <Typography variant='h2'> IQAC CELL</Typography>
+      <Header />
     </div>
   );
 }
