d2099dc1f1636930806f0d337db0bd8e77431a80
Kirtanaaa <uppalakirtana@gmail.com>
2023-12-28 19:45:40 +0530

    routed qat and programmes offered

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
-
-import AboutMain from './components/Header/About/AboutMain'
+import AboutMain from './components/Header/About/AboutMain';
 import IQAC from './components/Header/About/IQAC/IQAC';
 import JNTUGV from './components/Header/About/JNTUGV/JNTUGV';
 import Director from './components/Header/About/DIRECTOR/Director';
 import { Route, Routes } from 'react-router-dom';
-// import QAT from './components/Header/QAT/QAT';
 import ProgrammesOffered from './components/Header/Programmes_offered/ProgrammesOffered';
+import QAT from './components/Header/QAT/QAT';
 
 function App() {
   return (
@@ -23,12 +22,12 @@ function App() {
           <Route path="about-jntugv" element={<JNTUGV />} />
           <Route path="about-director" element={<Director />} />
         </Route>
+        <Route path="/programmes-offered" element={<ProgrammesOffered />} />
+        <Route path="/qat" element={<QAT />} />
       </Routes>
 
-      {/*<QAT/>*/}
-      <ProgrammesOffered/>
-      
 
+    
       <Footer />
     </div>
   );
