6c864a375da7ef0f46a5d9b23d2f07060bb9c707
SUMASRI BHOGI <sumasribhogi.project@gmail.com>
2023-12-28 14:21:14 +0530

    updated about page

--- src/components/Header/About/IQAC/IQAC.js
+++ src/components/Header/About/IQAC/IQAC.js
@@ -6,31 +6,109 @@ function IQAC() {
   return (
     <div className="ABOUTmainContainer">
       <div className="allRightContent">
-        <div className="allRightContentHeading">About JNTUGV</div>
+        <div className="allRightContentHeading">About IQAC</div>
 
         <div className="allRightContentProfile">
           {/* Profile information */}
           <center></center>
           <hr />
           {/* Profile description */}
+          <h2 className="intro">Introduction</h2>
+          <Typography>The IQAC is meant for planning, guiding and monitoring Quality Assurance (QA) 
+            and Quality Enhancement (QE) activities of the University. The IQAC may channelize and 
+            systematize the efforts and measures of an institution towards academic excellence. 
+            It should not be yet another hierarchical structure or record-keeping exercise in the University; 
+            it would be a facilitative and participative organ of the University. The IQAC should become a driving 
+            force for ushering in quality by working out intervention strategies to remove deficiencies and enhance quality.</Typography><br/>
+
+            <Typography>Internal Quality Assurance Cell (IQAC) to periodically assess and suggest measures to upgrade the 
+            quality in the overall performance of an academic institution. With this objective in mind JNTUK- University 
+            College of Engineering, Vizianagaram has formed an IQAC cell to ensure continuous quality enhancement and 
+            sustenance in its academic as well administrative activities. As quality improvement is a continuous process 
+            the IQAC has been playing a vital role in the post-accreditation phase of JNTUK- University College of Engineering, 
+            Vizianagaram as it is committed to achieve academic excellence.</Typography><br/>
+
+            <Typography>JNTUK- University College of Engineering, Vizianagaram has established the Internal Quality Assurance Cell (IQAC) 
+            as a post accreditation quality sustenance measure. The Cell comprises of different committees to assess, monitor, 
+            and advise on different quality aspects of the overall functioning of the University.</Typography>
+          
+          <h2 className="vision">Vision</h2>
           <Typography>
-            JNTU College of Engineering, Vizianagaram was established in the
-            year 2007 as a constituent College of JNTU Hyderabad.JNTU Hyderabad
-            was trifurcated into three Universities by the Andhra Pradesh Act
-            No. 30 of 2008 and since 24th August 2008, the College has become
-            the constituent college of JNTU Kakinada. Vide University Act No.22
-            of 2021, JNTU Kakinada is bifurcated and Jawaharlal Nehru
-            Technological University Gurajada, Vizianagaram come into existence
-            as a separate University vide G.O.Ms.No.3, dated: 12-01-2022.
-            <Typography>
-              The university is spread across six districts i.e
-              Vizianagaram,Visakhapatnam, Srikakulam, Parvathipuram Manyam,
-              Alluri Sitharama Raju and Anakapalli. There are 2 constituent
-              colleges and 37 affiliated colleges under its jurisdiction and
-              catering education in different Engineering, Pharmacy and
-              Management departments.
-            </Typography>
+          To create the bench mark in defining the quality of JNTUK-UCEV as a professional higher education in Engineering and 
+          Technology in quality evaluation in teaching, learning, research & development, a quality culture with conscious, reliable 
+          and transparent efforts through a combination of self and external quality evaluation, promotion and sustenance of the 
+          institutional and human values to meet global standards</Typography>
+
+          <h2 className="mission">Mission</h2>
+          <Typography>❖ To channelize and systematize the efforts and measures towards academic excellence in order to enhance quality.<br />
+          ❖ To encourage departments to establish student Learning-centric environment to use ICT tools for teaching and learning process and to &nbsp;inculcate value–based education system.<br />
+          ❖ To encourage Departments to organize Seminars, Workshops, Orientation and Faculty Development Program.<br />
+          ❖ To ensure an effective and transparent operations to achieve excellence in Academic, Administrative and Financial activities.<br />
+          ❖ To develop an Institutional MIS this will provide the quality information about the institution.<br />
+          ❖ To ensure the adequacy, timely maintenance and smooth functioning of the support structure.<br />
+          ❖ To build association with stakeholders and to stimulate Social Responsibility.
           </Typography>
+
+          <h2 className="ss">Strategies & objectives</h2>
+          <Typography>
+          ❖ Development and application of quality benchmarks/parameters academic and administrative activities of the University.<br />
+          ❖ Facilitating the creation of a learner-centric environment conducive for quality education and faculty maturation to adopt the required knowledge and technology for participatory teaching and learning process.<br />
+          ❖ Arrangement for feedback responses from students, parents and other stakeholders on quality related institutional processes.<br />
+          ❖ Dissemination of information on the various quality parameters of higher education.<br />
+          ❖ Organization of inter and intra University workshops, seminars on quality related themes and promotion of quality circles.<br />
+          ❖ Documentation of the various events /activities of the University, leading to quality improvement.<br />
+          ❖ Acting as a nodal agency of the University for coordinating quality-related activities, including adoption and dissemination of good practices.<br />
+          ❖ Development and maintenance of Institutional database through MIS for the purpose of maintaining /enhancing the institutional quality.<br />
+          ❖ To develop a system for conscious, consistent and catalytic action to improve the academic and administrative performance of the institution.<br />
+          ❖ To promote measures for institutional functioning towards quality enhancement through internalization of quality culture and institutionalization of best practices.<br />
+          ❖ To ensure an enhancement and integration among the various activities of the university and institutionalize many good practices<br />
+          </Typography>
+
+          <h2 className="func">Functions</h2>
+          <Typography>❖ Developing ethical work culture in JNTUK-UCEV.<br />
+          ❖ Establish and implement quality benchmarks for academic and administrative activities.<br />
+          ❖ Plays a vital role in the Institute for coordinating various activities ensuring quality including adoption and dissemination of best methods and practices.<br />
+          ❖ Organizing inter and intra Institutional Workshops, Seminars, awareness programs, Project Exhibition and Research activities.<br />
+          ❖ Ensuring a learner-centric ambience conducive for quality education and for continual improvement.<br />
+          ❖ Emphasize faculty expertise to adopt the knowledge, technology and innovations for participatory teaching and learning process.<br />
+          ❖ Conduct periodic audits of academic, administrative and research activities for quality assessment.<br />
+          ❖ Collection of feedback from different stakeholders, Analyze and suggest remedial actions for all stakeholder satisfaction.
+          ❖ Preparing Annual Quality Assurance Report (AQAR) as per norms.</Typography>
+
+          <h2 className="ben">Benefits</h2>
+          <Typography>
+
+          ❖ Ensure heightened level of clarity and focus in institutional functioning towards quality enhancement.<br />
+          ❖ Ensure internationalization of the quality culture.<br />
+          ❖ Ensure heightened level of clarity and focus in institutional functioning towards quality enhancement and Ensure internalization of the quality culture.<br />
+          ❖ Ensure enhancement and integration among the various activities of the institution and institutionalize good practices.<br />
+          ❖ Provide a sound basis for decision-making to improve institutional functioning.<br />
+          ❖ Act as a dynamic system for quality changes in the University.<br />
+          ❖ Build an organized methodology of documentation and internal communication.
+          </Typography>
+
+          <h2 className="features">Operational features of the IQAC</h2>
+          <Typography>❖ Development and application of quality benchmarks/parameters for the various academic and administrative activities of the University.<br />
+          ❖ Facilitating the creation of a learner-centric environment conducive for quality education and faculty maturation to adopt the required knowledge and technology for participatory teaching and learning process.<br />
+          ❖ Arrangement for feedback responses from students, parents and other stakeholders on quality related institutional processes.<br />
+          ❖ Dissemination of information on the various quality parameters of higher education.<br />
+          ❖ Organization of inter and intra University workshops, seminars on quality related themes and promotion of quality circles.<br />
+          ❖ Documentation of the various events /activities of the University, leading to quality improvement.<br />
+          ❖ Acting as a nodal agency of the University for coordinating quality-related activities, including adoption and dissemination of good practices.<br />
+          ❖ Development and maintenance of Institutional database through MIS for the purpose of maintaining /enhancing the institutional quality.<br />
+          ❖ Development of Quality Culture in University.
+         </Typography>
+
+         <h2 className="mm">Monitoring Mechanism</h2>
+         <Typography>
+         ❖ Preparation of the Annual Quality Assurance Report (AQAR) of the University based on the quality parameters/assessment criteria developed by the relevant quality assurance body (like NAAC, NBA, AB) in the prescribed format.<br />
+         ❖ Bi-annual development of Quality Radars (QRs) and Ranking of Integral Units of HEIs based on the AQAR.<br />
+         ❖ Interaction with SQACs in the pre and post accreditation quality assessment, sustenance and enhancement endeavors.<br />
+         ❖ The AQAR shall be approved by the statutory bodies of the University (such as Syndicate, Governing Council/Board) for the follow up action for necessary quality enhancement measures.<br />
+         ❖ The Universities shall regularly submit the AQARs to the NAAC/other accreditation bodies.<br />
+         ❖ All Universities shall submit AQARs and/or Quality Radars (QRs) and follow up reports of AQARs to the UGC.<br />
+         </Typography>
+
         </div>
       </div>
     </div>
@@ -38,3 +116,11 @@ function IQAC() {
 }
 
 export default IQAC;
+
+
+
+
+
+
+
+
