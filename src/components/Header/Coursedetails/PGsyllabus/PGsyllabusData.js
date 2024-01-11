e03e574e4a489cd9449530aae506b4b3db99e00a
Kirtanaaa <uppalakirtana@gmail.com>
2024-01-11 14:10:25 +0530

    pgsyllabus

--- src/components/Header/Coursedetails/PGsyllabus/PGsyllabusData.js
+++ src/components/Header/Coursedetails/PGsyllabus/PGsyllabusData.js
@@ -1,166 +1,169 @@
+// Import your PDF files
+import itmca20 from './r20/itmca.pdf';
+import itmtech20 from './r20/itmtech.pdf';
 
-import itmca20 from "./r20/mca.pdf";
-import itmtech20 from "./r20/mtech.pdf";
-import cse19 from "./r19/cse.pdf";
-import ece19 from "./r19/ece.pdf";
-import eee19 from "./r19/eee.pdf";
-import mechanical19 from "./r19/mechanical.pdf";
-import it19 from "./r19/it.pdf";
-import met19 from "./r19/met.pdf";
-import cse16 from "./r16/cse.pdf";
-import ece16 from "./r16/ece.pdf";
-import eee16 from "./r16/eee.pdf";
-import mechanical16 from "./r16/mech.pdf";
-import it16 from "./r16/it.pdf";
-import itmca16 from "./r16/it-mca.pdf";
-import cse13 from "./r13/cse.pdf";
-import ece13 from "./r13/ece.pdf";
-import eee13 from "./r13/eee.pdf";
-import mechanical13 from "./r13/mech.pdf";
-import it13 from "./r13/it.pdf";
-import itmca13 from "./r13/it-mca.pdf";
-const UGSyllabusData = [
+import cse19 from './r19/cse.pdf';
+import ece19 from './r19/ece.pdf';
+import eee19 from './r19/eee.pdf';
+import it19 from './r19/it.pdf';
+import mech19 from './r19/mech.pdf';
+import met19 from './r19/met.pdf';
+
+import cse16 from './r16/cse.pdf';
+import ece16 from './r16/ece.pdf';
+import eee16 from './r16/eee.pdf';
+import it16 from './r16/it.pdf';
+import itmca16 from './r16/itmca.pdf';
+import mech16 from './r16/mech.pdf';
+
+import cse13 from './r13/cse.pdf';
+import ece13 from './r13/ece.pdf';
+import eee13 from './r13/eee.pdf';
+import it13 from './r13/it.pdf';
+import mca13 from './r13/mca.pdf';
+import mech13 from './r13/mech.pdf';
+
+const PGSyllabusData = [
   {
     sno: 1,
-    department: 'Information Technology',
-    Specialization: 'Master of Computer Applications (MCA)',
+    department: 'Information Technology (MCA)',
     pdfPath: itmca20,
+    specialization: 'Master of Computer Applications (MCA)',
     name: 'View/Download',
   },
   {
     sno: 2,
-    department: 'Information Technology',
-    Specialization: 'M.Tech - Data Science',
-    pdfPath:itmtech20,
+    department: 'Information Technology (M.Tech)',
+    pdfPath: itmtech20,
+    specialization: 'M.Tech - Data Science',
     name: 'View/Download',
   },
   {
     sno: 3,
-    department: 'Computer Science and Engineering',
-    Specialization: 'Advanced Power Systems',
+    department: ' Electrical & Electronics Engineering',
     pdfPath: cse19,
-    name: 'Vi3ew/Download',
+    specialization: 'Advanced Power Systems',
+    name: 'View/Download',
   },
   {
     sno: 4,
-    department: 'Electronics & Communication Engineering',
-    Specialization: 'Thermal Engineering',
-    pdfPath:ece19,
+    department: 'Mechanical Engineering',
+    pdfPath: ece19,
+    specialization: 'Thermal Engineering',
     name: 'View/Download',
   },
   {
     sno: 5,
-    department: 'Electrical & Electronics Engineering',
-    Specialization: 'Systems & Signal Processing',
+    department: 'Electronics & Communication Engineering',
     pdfPath: eee19,
+    specialization: 'Systems & Signal Processing',
     name: 'View/Download',
   },
   {
     sno: 6,
-    department: 'Information Technology',
-    Specialization: 'Computer Science & Engineering ',
+    department: 'Computer Science & Engineering',
     pdfPath: it19,
+    specialization: 'Computer Science & Engineering ',
     name: 'View/Download',
   },
   {
     sno: 7,
-    department: 'Metallurgical Engineering',
-    Specialization: 'Master of Computer Applications (MCA)',
-    pdfPath: met19,
+    department: 'Information Technology (MCA)',
+    pdfPath: mech19,
+    specialization: 'Master of Computer Applications (MCA)',
     name: 'View/Download',
   },
   {
     sno: 8,
-    department: 'Mechanical Engineering',
-    Specialization: 'Material Science and Technology',
-    pdfPath:  mechanical19,
+    department: 'Metallurgical Engineering',
+    pdfPath: met19,
+    specialization: 'Material Science and Technology',
     name: 'View/Download',
   },
   {
     sno: 9,
     department: 'Electrical & Electronics Engineering',
-    Specialization: 'Advanced Power Systems',
-    pdfPath: eee16,
+    pdfPath: cse16,
+    specialization: 'Advanced Power Systems',
     name: 'View/Download',
   },
   {
     sno: 10,
     department: 'Mechanical Engineering',
-    Specialization: 'Machine Design ',
-    pdfPath: mechanical16,
+    pdfPath: ece16,
+    specialization: 'Machine Design',
     name: 'View/Download',
   },
   {
-    sno:11,
+    sno: 11,
     department: 'Electronics & Communication Engineering',
-    Specialization: 'Systems & Signal Processing',
-    pdfPath: ece16,
+    pdfPath: eee16,
+    specialization: 'Systems & Signal Processing',
     name: 'View/Download',
   },
   {
     sno: 12,
-    department: 'Computer Science and Engineering',
-    Specialization: 'Computer Science & Engineering ',
-    pdfPath: cse16,
+    department: 'Computer Science & Engineering',
+    pdfPath: it16,
+    specialization: 'Computer Science & Engineering ',
     name: 'View/Download',
   },
   {
     sno: 13,
     department: 'Information Technology',
-    Specialization: 'Information Technology',
-    pdfPath: it16,
+    pdfPath: itmca16,
+    specialization: 'Information Technology',
     name: 'View/Download',
   },
   {
     sno: 14,
-    department: 'Information Technology',
-    Specialization: ' Master of Computer Applications (MCA)',
-    pdfPath: itmca16,
+    department: 'Information Technology (MCA)',
+    pdfPath: mech16,
+    specialization: 'Master of Computer Applications (MCA)',
     name: 'View/Download',
   },
   {
     sno: 15,
-    department: 'Information Technology',
-    Specialization: 'Advanced Power Systems',
-    pdfPath: itmca13,
+    department: 'Electrical & Electronics Engineering',
+    pdfPath: cse13,
+    specialization: 'Advanced Power Systems',
     name: 'View/Download',
   },
   {
     sno: 16,
-    department: 'Electrical & Electronics Engineering',
-    Specialization: 'Machine Design ',
-    pdfPath: eee13,
+    department: 'Mechanical Engineering',
+    pdfPath: ece13,
+    specialization: 'Machine Design ',
     name: 'View/Download',
   },
   {
     sno: 17,
-    department: 'Mechanical Engineering',
-    Specialization: 'Systems & Signal Processing',
-    pdfPath: mechanical13,
+    department: 'Electronics & Communication Engineering',
+    pdfPath: eee13,
+    specialization: 'Systems & Signal Processing',
     name: 'View/Download',
   },
   {
     sno: 18,
-    department: 'Electronics & Communication Engineering',
-    Specialization: 'Computer Science & Engineering ',
-    pdfPath: ece13,
+    department: 'Computer Science & Engineering',
+    pdfPath: it13,
+    specialization: 'Computer Science & Engineering',
     name: 'View/Download',
   },
   {
     sno: 19,
-    department: 'Computer Science and Engineering',
-    Specialization: 'Information Technology',
-    pdfPath: cse13,
+    department: 'Information Technology',
+    pdfPath: mca13,
+    specialization: 'Information Technology',
     name: 'View/Download',
   },
   {
     sno: 20,
-    department: 'Information Technology',
-    Specialization: 'Master of Computer Applications (MCA)',
-    pdfPath: it13,
+    department: 'Information Technology (MCA)',
+    pdfPath: mech13,
+    specialization: 'Master of Computer Applications (MCA)',
     name: 'View/Download',
   },
- 
 ];
 
-export default UGSyllabusData;
+export default PGSyllabusData;
